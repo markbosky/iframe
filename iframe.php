b5c95591cd86bcaf1d1adf3db685e6f27df551ad
Mark Bosky <mark.bosky@oa.mo.gov>
2017-11-02 14:37:48 -0500

    adding additional "title" parameter/attribute for accessibility improvement

--- iframe.php
+++ iframe.php
@@ -2,7 +2,7 @@
 /*
 Plugin Name: iframe
 Plugin URI: http://wordpress.org/plugins/iframe/
-Description: [iframe src="http://www.youtube.com/embed/4qsGTXLnmKs" width="100%" height="500"] shortcode
+Description: [iframe src="https://www.youtube.com/embed/zCj1FPATv0g" title="Youtube Video" width="100%" height="500"] shortcode
 Version: 4.3
 Author: webvitaly
 Author URI: http://web-profile.com.ua/wordpress/plugins/
@@ -13,11 +13,12 @@ define('IFRAME_PLUGIN_VERSION', '4.3');
 
 function iframe_plugin_add_shortcode_cb( $atts ) {
 	$defaults = array(
-		'src' => 'http://www.youtube.com/embed/4qsGTXLnmKs',
+		'src' => 'https://www.youtube.com/embed/zCj1FPATv0g',
 		'width' => '100%',
 		'height' => '500',
 		'scrolling' => 'yes',
-		'class' => 'iframe-class',
+		'title' => 'Embedded Content',
+		'class' => 'iframe-class embed-responsive-item',
 		'frameborder' => '0'
 	);
 
@@ -57,18 +58,3 @@ function iframe_plugin_add_shortcode_cb( $atts ) {
 	return $html;
 }
 add_shortcode( 'iframe', 'iframe_plugin_add_shortcode_cb' );
-
-
-function iframe_plugin_row_meta_cb( $links, $file ) {
-	if ( $file == plugin_basename( __FILE__ ) ) {
-		$row_meta = array(
-			'support' => '<a href="http://web-profile.com.ua/wordpress/plugins/iframe/" target="_blank"><span class="dashicons dashicons-editor-help"></span> ' . __( 'Iframe', 'iframe' ) . '</a>',
-			'donate' => '<a href="http://web-profile.com.ua/donate/" target="_blank"><span class="dashicons dashicons-heart"></span> ' . __( 'Donate', 'iframe' ) . '</a>',
-			'iframe_pro' => '<a href="http://codecanyon.net/item/advanced-iframe-pro/5344999?ref=webvitalii" target="_blank"><span class="dashicons dashicons-star-filled"></span> ' . __( 'Advanced iFrame Pro', 'iframe' ) . '</a>',
-			'pro' => '<a href="http://codecanyon.net/item/silver-bullet-pro/15171769?ref=webvitalii" target="_blank" title="Speedup and protect WordPress in a smart way"><span class="dashicons dashicons-star-filled"></span> ' . __( 'Silver Bullet Pro', 'iframe' ) . '</a>'
-		);
-		$links = array_merge( $links, $row_meta );
-	}
-	return (array) $links;
-}
-add_filter( 'plugin_row_meta', 'iframe_plugin_row_meta_cb', 10, 2 );
